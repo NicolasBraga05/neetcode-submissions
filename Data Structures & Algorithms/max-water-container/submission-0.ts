5e831c3219c034a5b373fab405cbcdcaf288d44a
Nicolas Braga <115794894+NicolasBraga05@users.noreply.github.com>
2026-09-24 12:23:22 -0300

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights: number[]): number {
+        let result = 0
+        let left = 0
+        let right = heights.length - 1
+
+        while(left < right) {
+            let diff = right - left 
+            let sum 
+            if(heights[left] < heights[right]){
+                sum = heights[left] * diff
+                result = Math.max(result, sum)
+                left++
+                console.log(sum)
+            } else if(heights[left] > heights[right]){
+                sum = heights[right] * diff
+                result = Math.max(result, sum)
+                right--
+            } else {
+                sum = heights[left] * diff
+                result = Math.max(result, sum)
+                left++
+            }
+        }
+
+        return result
+    }
+}
